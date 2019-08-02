2ba9e772615cda6762c86e0a24fcdcee2a79d4f2
poepping <matt.poepping@gmail.com>
2019-08-02 14:58:50 -0600

    haiku-app

---
+++ haiku-app/precache-manifest.56376693deee87b07cc78e358ff1e9e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad61bf5a783b642a62334f12bbb6f15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9064e7b0b6b42a632da1",
+    "url": "/static/css/main.e43d5718.chunk.css"
+  },
+  {
+    "revision": "d53a0fb760448d46fba0",
+    "url": "/static/js/2.3a2ba5f9.chunk.js"
+  },
+  {
+    "revision": "9064e7b0b6b42a632da1",
+    "url": "/static/js/main.b8375084.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2de676c8470f1340b49d2c51419b218",
+    "url": "/static/media/Smackover.e2de676c.ttf"
+  },
+  {
+    "revision": "07e130c8ebdd0afb6b2ae86d755b97f1",
+    "url": "/static/media/bg.07e130c8.jpg"
+  }
+]);
