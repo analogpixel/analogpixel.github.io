35957dcca08f8cee6b082b6f7a4feef4fb15585c
poepping <matt.poepping@gmail.com>
2019-11-01 14:21:40 -0600

    doodle

---
+++ dailyDoodles/box2d-bear-fountain.js
@@ -0,0 +1,75 @@
+// https://www.iforce2d.net/b2dtut/introduction
+
+var world;
+var pl = planck;
+
+var boxes = new Array();
+var tbear;
+
+function preload() {
+  tbear = loadImage("data/tbear.png");
+}
+
+function setup() {
+    createCanvas(800, 800);
+
+  tbear.resize(50,40);
+  world = planck.World({
+    gravity: planck.Vec2(0, -10)
+  });
+
+  world.on('remove-fixture', function(fixture) {
+    // remove fixture from ui
+    // console.log("CALLED", fixture);
+  });
+
+  // var ground = world.createBody();
+
+  for (var i=0; i < 60; i++) {
+   boxes.push(newBox());
+  }
+
+  updateFixtures();
+}
+
+function newBox() {
+    var box = world.createBody().setDynamic();
+    box.createFixture(pl.Box(0,0));
+    m = Math.floor(Math.random()*2) == 1 ? 1 : -1;
+    box.applyForce( planck.Vec2(m*random(430),m*random(900)), planck.Vec2(0,0) );
+     box.setAngularVelocity(m*random(90),m*random(90));
+    return box;
+}
+
+function updateFixtures() {
+  world.step(1/260);
+  // box.applyAngularImpulse(-20, -20);
+
+  for (var i=0; i < boxes.length; i++) {
+    var {x,y} =  boxes[i].getPosition() ;
+    var a = boxes[i].getAngle();
+
+    // console.log(x,y,a);
+    y = map(y, -1,1,  height,0);
+    x = map(x, -1,1, width, 0);
+
+    push();
+    translate(x,y);
+    rotate( radians(a) );
+    image(tbear,0,0);
+    //rect(0,0, 10,10);
+    pop();
+
+    if (y > height) { world.destroyBody(boxes[i]); boxes[i] =  newBox(); }    
+  }
+}
+
+function keyPressed() {
+
+  updateFixtures();
+}
+
+function draw() {
+  background(90,30);
+  updateFixtures();
+}
---
+++ dailyDoodles/sketch.js
@@ -0,0 +1,11 @@
+
+function setup() {
+    createCanvas(800, 800);
+
+    // point to a div
+    // var canvas = createCanvas(800, 800);
+    //canvas.parent('sketch'); 
+}
+ 
+function draw() {
+}
